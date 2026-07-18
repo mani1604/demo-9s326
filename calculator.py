25cc8e5d9d6831f7bb85847857f6260fcfa61268
mani1604 <manish_sharma1604@yahoo.com>
2026-07-18 14:59:33 +0530

    Added docker build and push to ci

--- calculator.py
+++ calculator.py
@@ -6,3 +6,10 @@ def subtract(a, b):
 
 def multiply(a, b):
     return a * b
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+print(add(num1, num2))
+print(subtract(num1, num2))
+print(multiply(num1, num2))
